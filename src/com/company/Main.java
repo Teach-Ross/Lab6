0e27472395a9e613dbd638b792c9d5eb70eb2dcb
rteach <ross.c.teach@gmail.com>
2017-02-03 16:57:30 -0500

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,12 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
     static Scanner scnr = new Scanner(System.in);
 
-    public static String getUserInput (String prompt) {
+    public static String getUserInput(String prompt) {
         String input = null;
 
         System.out.println(prompt);
@@ -20,13 +21,23 @@ public class Main {
         return input;
     }
 
-    public static boolean startsWithVowel (String word) {
+    public static boolean startsWithVowel(String word) {
         return "AEIOUaeiou".indexOf(word.charAt(0)) != -1;
     }
 
-    public static int indexOfVowel (String word) {
+    //excludes aposrophes (it doesn't yet)
+    public static boolean containsSymbolsNumbers(String word) {
+
+        if (!Pattern.matches("[a-zA-Z]+", word)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static int indexOfVowel(String word) {
         int index = -1;
-        for (int i=0; i< word.length(); i++) {
+        for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
             if ("AEIOUaeiou".indexOf(word.charAt(i)) != -1) {
                 return word.indexOf(c);
@@ -35,14 +46,15 @@ public class Main {
         return index;
     }
 
-    public static String convertWord (String word) {
+    public static String convertWord(String word) {
         int split = indexOfVowel(word);
         StringBuilder latinWord = new StringBuilder();
 
-        if(startsWithVowel(word)) {
+        if (startsWithVowel(word)) {
             latinWord.append(word).append("way ");
-        }
-        else {
+        } else if (containsSymbolsNumbers(word) || split == -1) {
+            latinWord.append(word).append(" ");
+        } else {
             latinWord.append(word.substring(split)).append(word.substring(0, split)).append("ay ");
         }
         return latinWord.toString();
@@ -55,24 +67,19 @@ public class Main {
         input = getUserInput("Enter a line of text:");
 
         input = input.toLowerCase();
-        System.out.println(startsWithVowel(input));
-        String[] words = input.split(" ");
+
+        String[] words = input.split("\\s+");
 
 
-        for (int i=0; i < words.length; i++){
+        for (int i = 0; i < words.length; i++) {
             output = convertWord(words[i]);
             System.out.print(output);
         }
 
 
-
-        }
-
-
-
-
-
-
-        // write your code here
     }
+
+
+    // write your code here
+}
 
