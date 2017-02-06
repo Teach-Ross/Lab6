9a920e5d7267c4f1603e71cc5582f617d3f8f244
rteach <ross.c.teach@gmail.com>
2017-02-06 08:58:45 -0500

    Next Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,14 +25,41 @@ public class Main {
         return "AEIOUaeiou".indexOf(word.charAt(0)) != -1;
     }
 
-    //excludes aposrophes (it doesn't yet)
     public static boolean containsSymbolsNumbers(String word) {
 
-        if (!Pattern.matches("[a-zA-Z]+", word)) {
+        if (!Pattern.matches("[a-zA-Z!.?',]+", word)) {
             return true;
-        }else{
+        } else {
             return false;
         }
+    }
+
+    public static String findPunctuation(String word) {
+        String punctuation = "";
+        for (int i = word.length() - 1; i > 0; i--) {
+            if (!Character.isLetter((word.charAt(i)))) {
+                punctuation = word.charAt(i) + punctuation;
+            } else {
+                break;
+            }
+        }
+        return punctuation;
+    }
+
+    public static boolean isFirstLetterUpper(String word) {
+        if (Character.isUpperCase(word.charAt(0))) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isWordAllUpper(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            if (Character.isLowerCase((word.charAt(i)))) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static int indexOfVowel(String word) {
@@ -48,17 +75,33 @@ public class Main {
 
     public static String convertWord(String word) {
         int split = indexOfVowel(word);
+        String punctuation = findPunctuation(word);
         StringBuilder latinWord = new StringBuilder();
 
-        if (startsWithVowel(word)) {
-            latinWord.append(word).append("way ");
-        } else if (containsSymbolsNumbers(word) || split == -1) {
-            latinWord.append(word).append(" ");
+        if (containsSymbolsNumbers(word)) {
+            latinWord.append(word);
+        } else if (startsWithVowel(word) || split == -1) {
+            latinWord.append(word.substring(0, word.length() - punctuation.length())).append("way").append(punctuation);
         } else {
-            latinWord.append(word.substring(split)).append(word.substring(0, split)).append("ay ");
+            latinWord.append(word.substring(split, word.length() - punctuation.length())).append(word.substring(0, split)).append("ay").append(punctuation);
         }
-        return latinWord.toString();
+
+        String newWord = latinWord.toString();
+
+        if (isWordAllUpper(word)) {
+            newWord = newWord.toUpperCase();
+        } else if (isFirstLetterUpper(word) && !startsWithVowel(word)) {
+                newWord = newWord.substring(0, 1).toUpperCase() + newWord.substring(1);
+                char c[] = newWord.toCharArray();
+                int num = (word.length() - split - punctuation.length());
+                c[num] = Character.toLowerCase(c[num]);
+                newWord = new String(c);
+
+        }
+
+        return newWord;
     }
+
 
     public static void main(String[] args) {
         String input;
@@ -66,14 +109,11 @@ public class Main {
 
         input = getUserInput("Enter a line of text:");
 
-        input = input.toLowerCase();
-
         String[] words = input.split("\\s+");
-
 
         for (int i = 0; i < words.length; i++) {
             output = convertWord(words[i]);
-            System.out.print(output);
+            System.out.print(output + " ");
         }
 
 
